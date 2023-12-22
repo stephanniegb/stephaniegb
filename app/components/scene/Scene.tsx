93addc4bec6fca6112904cdf37362377ca6a97d6
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2023-12-22 12:48:27 +0100

    WIP

--- app/components/scene/Scene.tsx
+++ app/components/scene/Scene.tsx
@@ -17,56 +17,26 @@ const Texture = ({ texture }) => {
     x: 0,
     y: 0,
   });
-  useFrame(({ mouse, camera }) => {
+  useFrame(({ mouse }) => {
     setOffset({
       /* current position of the mesh, where i  want it to go to, percentage of transfromation */
       x: THREE.MathUtils.lerp(offset.x, mouse.x, 0.1),
       y: THREE.MathUtils.lerp(offset.y, mouse.y, 0.1),
     });
-    // camera.position.x = THREE.MathUtils.lerp(
-    //   camera.position.x,
-    //   mouse.x * .5,
-    //   0.03
-    // );
   });
   const uniforms = useMemo(
     () => ({
-      u_intensity: {
+      u_Alpha: {
         value: 0.3,
       },
       u_time: {
         value: 0.0,
       },
-      uAlpha: {
-        value: 1.0,
-      },
     }),
     []
   );
-  /* to use the mouse coordinates of the window.. we have to normalize it make it 0,0 at the center */
 
-  // console.log(offset);
-
-  // useLayoutEffect(() => {
-  //   window.addEventListener("mousemove", (e) => {
-  //     setMouse({
-  //       x: (e.clientX / innerWidth) * 2 - 1,
-  //       y: -(e.clientY / innerHeight) * 2 + 1,
-  //     });
-  //   });
-  //   return () => {};
-  // }, []);
-
-  // useEffect(() => {
-  //   setOffset({
-  //     /* current position of the mesh, where i  want it to go to, percentage of transfromation */
-  //     x: THREE.MathUtils.lerp(offset.x, mouse.x, 0.1),
-  //     y: THREE.MathUtils.lerp(offset.y, mouse.y, 0.1),
-  //   });
-  // }, [mouse]);
   console.log(offset);
-
-  /* if you want to use use framer you hav to figure out a way to lerp you mouse coordinates to always be between 0-1 */
 
   return (
     <mesh position={[offset.x, offset.y * 2.9, 0]}>
@@ -77,7 +47,6 @@ const Texture = ({ texture }) => {
         uniforms={uniforms}
         wireframe={false}
       />
-      {/* <meshBasicMaterial attach="material" map={texture} /> */}
     </mesh>
   );
 };
@@ -86,8 +55,5 @@ const ImageMesh = ({ url }) => {
   return <Texture texture={texture} />;
 };
 export default ImageMesh;
-// export default function Scene() {
-//   return <Image url={images.imageOne} />;
-// }
 
 const Scene = () => {};
