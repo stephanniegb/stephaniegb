f136f62b8b64403723b600d2298828edb247c005
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-01-20 15:47:25 +0100

    Update Wave.tsx

--- svg/Wave.tsx
+++ svg/Wave.tsx
@@ -4,12 +4,13 @@ const Wave = () => {
       className="z-[1] relative "
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 320"
+      preserveAspectRatio="none"
       style={{
         width: "100%",
         height: "auto",
       }}
     >
-      {/* <defs>
+      <defs>
         <clipPath id="the-object">
           <path
             fill="#eae7e7"
@@ -27,11 +28,10 @@ const Wave = () => {
           <feColorMatrix type="saturate" values="0" />
           <feBlend in="SourceGraphic" in2="noisy" mode="multiply" />
         </filter>
-      </defs> */}
+      </defs>
 
       <path
         fill="#e7e5e5"
-        fillOpacity="1"
         d="M0,224L80,234.7C160,245,320,267,480,282.7C640,299,800,309,960,309.3C1120,309,1280,299,1360,293.3L1440,288L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
         filter="url(#noiseFilter)"
         clipPath="url(#the-object)"
