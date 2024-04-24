346a2c1c2b4d0bf3ffcfc9bec50bca817b5c1510
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-04-24 15:02:01 +0100

    Update ImageMesh.tsx

--- app/components/scene/ImageMesh.tsx
+++ app/components/scene/ImageMesh.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { shaderMaterial } from "@react-three/drei";
-import { extend, useFrame, useLoader } from "@react-three/fiber";
+import { extend, useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 import React, { useContext, useMemo, useRef, useState } from "react";
 import { ShaderContext } from "@/app/context/ShaderContext";
@@ -20,27 +20,15 @@ const ImageShader = shaderMaterial(
 extend({ ImageShader });
 
 function ImageMesh({ url }: { url: string }) {
-  const [canvasOffset, setCanvasOffset] = useState({
-    x: 0,
-    y: 0,
-  });
-
   const { offset, mouse } = useContext(ShaderContext);
   const { cursor } = useContext(GlobalContext);
 
   const mesh = useRef(null);
   const [texture] = useMemo(() => useLoader(THREE.TextureLoader, [url]), [url]);
 
-  useFrame(({ mouse }) => {
-    setCanvasOffset({
-      x: THREE.MathUtils.lerp(canvasOffset.x, mouse.x, 0.1),
-      y: THREE.MathUtils.lerp(canvasOffset.y, mouse.y, 0.1),
-    });
-  });
-
   return (
     <mesh position={[mouse.x * 2.9, mouse.y * 4, 0]}>
-      <planeGeometry attach="geometry" args={[2.8, 1.8]} />
+      <planeGeometry attach="geometry" args={[2.8, 1.8, 16, 16]} />
       <imageShader
         ref={mesh}
         attach="material"
