ee99818ca1de9fc694a444754bc243d8827739a7
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-12-04 21:48:30 +0100

    refactor: removed unwanted code

--- app/components/scene/assets/images.ts
+++
@@ -1,9 +0,0 @@
-const images = {
-  imageOne: "/projects/image-7.jpg",
-  imageTwo: "/projects/image-1.jpg",
-  imageThree: "/projects/image-6.jpg",
-  imageFour: "/projects/image-4.jpg",
-  imageFive: "/projects/image-3.jpg",
-};
-
-export default images;
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,7 @@
-"use client";
-import { useContext, useEffect, useRef, useState } from "react";
-import { motion as m, useScroll } from "framer-motion";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
-import Hero from "./components/hero/Hero";
-import About from "./components/About/About";
-import Cursor from "./components/cursor/Cursor";
-import Experience from "./components/experience/Experience";
-import Work from "./components/work/Work";
-import Wave from "@/svg/Wave";
-import PseudoLoader from "./components/loader/PseudoLoader";
-import GlobalContext from "./context/GlobalContext";
-
 export default function Home() {
-  const { opaque } = useContext(GlobalContext);
-  const [loader, setLoader] = useState(true);
-  const grandParentRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: grandParentRef,
-    offset: ["0 1", "1 1"],
-  });
-
-  const handleBeforeUnload: () => void = () => {
-    document.body.style.display = "none";
-    window.scrollTo(0, 0);
-  };
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLoader(false);
-    }, 3000);
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <main className="relative">
-      {loader ? (
-        <PseudoLoader />
-      ) : (
-        <>
-          <Cursor />
-          <Navbar />
-          <m.div
-            className={`${
-              opaque ? "opacity-100" : "opacity-0"
-            } transition-[opacity] duration-1000
-             relative z-[1]`}
-            ref={grandParentRef}
-          >
-            <div className="bg-noise-bg z-[1] bg-offWhite">
-              <Hero />
-              <About />
-              <Experience />
-              <Work />
-            </div>
-            <Wave />
-            <Footer scrollYProgress={scrollYProgress} />
-          </m.div>
-        </>
-      )}
+      <h1>portfolio</h1>
     </main>
   );
 }
