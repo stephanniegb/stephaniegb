0d5afb9c24171f136433a775e37cc55317b7d744
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-12-08 14:46:04 +0100

    chore: update open graph meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,9 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Stephanie Egbuonu",
   description:
-    "Stephanie Egbuonu is a Front End Software Engineer and this is her portfolio",
+    "Stephanie Egbuonu is a Frontend Software Engineer and this is her portfolio",
   icons: {
     icon: "./favicon.ico",
-    apple: "./apple-touch-icon.png",
   },
 };
 
